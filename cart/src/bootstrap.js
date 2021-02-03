c8100245d73ccbcee494a1e675d6870292eae779
Tatiana Grigorieva <tsg1204@gmail.com>
2021-02-02 19:15:12 -0500

    refactor container

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,5 +1,18 @@
 import faker from 'faker';
 
-const cartText = `<div>You have ${faker.random.number()} items in your cart. </div>`;
+const mount = (el) => {
+  const cartText = `<div>You have ${faker.random.number()} items in your cart. </div>`;
 
-document.querySelector('#dev-cart').innerHTML = cartText;
+  el.innerHTML = cartText;
+  //ReactDOM.render(..., el);
+};
+
+if (process.env.NODE_ENV === 'development') {
+  const el = document.querySelector('#dev-cart');
+
+  if (el) {
+    mount(el);
+  }
+}
+
+export { mount };
